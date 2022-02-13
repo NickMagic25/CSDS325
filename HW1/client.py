e812689e271db2187e51c00bcbd5b0d647166a6d
NickMagic25 <37313679+NickMagic25@users.noreply.github.com>
2022-02-13 15:36:31 -0500

    Added comments, fixed issue where server would not reconnect after client terminates, README file added

--- HW1/client.py
+++ HW1/client.py
@@ -1,9 +1,9 @@
 #Nick Majkic
 #CSDS 325
-
+import socket
 from socket import *
 
-
+# just to make it easier to get the string #
 def help():
     return """
     help 
@@ -15,26 +15,32 @@ def help():
     bye """
 
 
-def connect():
+def main():
     print("Welcome to the KeyValue Service Client")
-    serverName=raw_input("Type server name or IP address: ")
-    serverPort=12000
-    clientSocket=socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName, serverPort))
-    loop=True
-    while loop:
-        command=raw_input("KeyValue Service> ")
-        if command.lower() == "bye":
-            clientSocket.send(command.lower().encode())
-            clientSocket.close()
-            loop=False
-            print("See you later.")
-        elif command.lower() == "help":
-            print(help())
-        else:
-            clientSocket.send(command.encode())
-            serverOuput=clientSocket.recv(1024)
-            print(serverOuput.decode())
+    # taken from chapter 2 slides #
+    try:
+        serverName=raw_input("Type server name or IP address: ")
+        serverPort=12000
+        clientSocket=socket(AF_INET, SOCK_STREAM)
+        clientSocket.connect((serverName, serverPort))
+        loop = True  # the loop will always run while loop is true #
+        print("Connected to " + serverName + "!")
+        while loop:
+            command = raw_input("KeyValue Service> ")
+            if command.lower() == "bye":  # check if bye was sent #
+                clientSocket.send(command.lower().encode())
+                clientSocket.close()  # close the connection #
+                loop = False  # terminate the loop #
+                print("See you later.")
+            elif command.lower() == "help":  # check if help was sent #
+                print(help())
+            else:  # else send the command to the server
+                clientSocket.send(command.encode())
+                serverOuput = clientSocket.recv(1024)
+                print(serverOuput.decode())
+    except gaierror:  # if the server name/IP address is invalid catch the error and run main again #
+        print("Invalid server name or IP address \n")
+        main()
 
 
-connect()
+main()  # run main function #
--- HW1/server.py
+++ HW1/server.py
@@ -3,9 +3,10 @@
 from socket import *
 
 
-store_inventory={}
+store_inventory={}  # store inventory for the whole program #
 
 
+# get function, get value for a given key #
 def get(key):
     if key in store_inventory:
         return str(store_inventory[key])
@@ -13,14 +14,13 @@ def get(key):
         return "Cannot find \"" + key + "\" in the data store."
 
 
+# put function, maps a key to a value #
 def put(key,value):
-    if not isinstance(value,int):
-        return "Value \"" + value + "\" is not valid."
-    else:
-        store_inventory[key]=value
-        return "Successfully mapped \""+ str(value) + "\" to \"" + key + "\"."
+    store_inventory[key]=value
+    return "Successfully mapped \""+ str(value) + "\" to \"" + key + "\"."
 
 
+# returns the keys with their values #
 def mappings():
     output=""
     for key in list(store_inventory):
@@ -28,13 +28,15 @@ def mappings():
     return output
 
 
-def keyset():
+# returns all keys #
+def key_set():
     output=""
     for key in list(store_inventory):
         output+=key + "\n"
     return output
 
 
+# returns all values #
 def values():
     output = ""
     for key in list(store_inventory):
@@ -42,36 +44,51 @@ def values():
     return output
 
 
+# handles user input #
 def handler(arr):
-    if len(arr)==3:
+    if len(arr) == 3:
         if arr[0].lower() == "put":
-            return put(arr[1], int(arr[2]))
-    elif len(arr)==2:
+            try:  # map arr[2] to arr[1] if arr[2] is an int
+                return put(arr[1], int(arr[2]))
+            except ValueError as e:  # catch ValueError in case arr[2] is not an int #
+                return "\"" + arr[2] + "\" is not a valid value"
+    elif len(arr) == 2:
         if arr[0].lower() == "get":
-            return get(arr[1])
+            return get(arr[1])  # get the value at arr[1]#
     elif len(arr)==1:
         if arr[0].lower() == "values":
-            return values()
+            return values()  # get all values #
         elif arr[0].lower() == "keyset":
-            return keyset()
+            return key_set()  # get the keyset #
         elif arr[0].lower() == "mappings":
-            return mappings()
-    return "Invalid command"
+            return mappings()  # get the mappings #
+    return "Invalid command"  # whatever was entered is an invalid command #
 
+
+def connected(serverSocket):
+    connectionSocket, addr = serverSocket.accept()
+    loop = True
+    try:
+        while loop:  # always run the loop #
+            sentence = connectionSocket.recv(1024).decode()
+            print("Received: " + sentence)  # prints everything recieved, makes checking for errors easier #
+            arr = sentence.split()
+            if arr[0].lower() == "bye":  # check if input was bye #
+                connectionSocket.close()
+            else:  # else handle the input #
+                connectionSocket.send(handler(arr).encode())  # send back what the handler determines #
+    # kept getting socket.error on terminating client so this is how I'm handling it #
+    except error:  # catch socket.error and pass #
+        connected(serverSocket)  # allows for the server to be reconnected after termination #
 
 def main():
+    # connect to client, taken from chapter 2 slides #
     serverPort = 12000
     serverSocket = socket(AF_INET,SOCK_STREAM)
     serverSocket.bind(('',serverPort))
     serverSocket.listen(1)
     print ("The server is ready to receive")
-    connectionSocket, addr = serverSocket.accept()
-    while True:
-        sentence = connectionSocket.recv(1024).decode()
-        print("Recieved: " + sentence)
-        arr=sentence.split()
-        if arr[0] =="bye":
-            connectionSocket.close();
-        connectionSocket.send(handler(arr).encode())
+    connected(serverSocket)  # function to connect to client #
 
-main()
+
+main()  # run the main function #
